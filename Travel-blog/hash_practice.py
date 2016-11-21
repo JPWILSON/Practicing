f30e35a6db92bbff1a1f56c9460c9861d138601c
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2016-11-21 09:46:59 +0200

    Feat: Update password hashing functionality

--- Travel-blog/hash_practice.py
+++ Travel-blog/hash_practice.py
@@ -4,3 +4,9 @@ def using_md5(word2hash):
 	x = hashlib.md5(word2hash).hexdigest()
 	return x
 
+def using_sha2(word):
+	y = hashlib.sha256(word).hexdigest()
+	return y
+
+
+print using_sha2("udacity")
